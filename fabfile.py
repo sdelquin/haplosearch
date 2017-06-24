694c21de9ad55a6ed391d81372df8d15a86b5008
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-06-24 15:44:25 +0100

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import env, local, prefix, cd, run
+
+env.hosts = ["haplosearch.com"]
+
+
+def deploy():
+    local("git push")
+    with prefix("source ~/.virtualenvs/haplosearch/bin/activate"):
+        with cd("~/haplosearch"):
+            run("git pull")
+            run("pip install -r requirements.txt")
+            #run("bower install")
+            #run("python manage.py collectstatic --noinput")
+            #run("supervisorctl restart haplosearch")
