82ca48728e038606722d160cbd970376a7ae9f95
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-17 20:30:05 +0100

    Fix fabric with new Pipenv

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,13 @@
-from fabric.api import env, local, prefix, cd, run
+from fabric.api import env, local, cd, run
 
-env.hosts = ["haplosearch.com"]
+env.hosts = ['haplosearch.com']
 
 
 def deploy():
-    local("git push")
-    with prefix("source ~/.virtualenvs/haplosearch/bin/activate"):
-        with cd("~/haplosearch"):
-            run("git pull")
-            run("pip install -r requirements.txt")
-            run("bower install")
-            run("python manage.py collectstatic --noinput")
-            run("supctl restart haplosearch")
+    local('git push')
+    with cd('~/haplosearch'):
+        run('git pull')
+        run('pipenv install')
+        run('bower install')
+        run('pipenv run python manage.py collectstatic --noinput')
+        run('supctl restart haplosearch')
