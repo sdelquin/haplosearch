6159d70595e1848e6275ac48d73bbacaaa80cc5a
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-04-11 17:02:51 +0100

    Migrate from pipenv to requirements

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,15 @@
-from fabric.api import env, local, cd, run
+from fabric.api import env, local, cd, run, prefix
 
 env.hosts = ['cloud']
 
 
 def deploy():
     local('git push')
-    with cd('~/haplosearch'):
-        run('git pull')
-        run('pipenv install')
-        with cd('app/static'):
-            run('npm install')
-        run('pipenv run python manage.py collectstatic --noinput')
-        run('supervisorctl restart haplosearch')
+    with prefix('source ~/.virtualenvs/haplosearch/bin/activate'):
+        with cd('~/code/haplosearch'):
+            run('git pull')
+            run('pip install -r requirements.txt')
+            with cd('app/static'):
+                run('npm install')
+            run('python manage.py collectstatic --noinput')
+            run('supervisorctl restart haplosearch')
